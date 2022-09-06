daf541a28f5316ffeca2b4910e65df60e3148912
jackyniu <jackyniu@tencent.com>
2022-09-06 17:49:04 +0800

    pycharm 代码补全不生效问题

--- demo1/intro.py
+++ demo1/intro.py
@@ -1,11 +1,20 @@
 import pgzrun
+from pgzero.actor import Actor
 
-WIDTH = 300
-HEIGHT = 300
+alien = Actor('alien')
+alien.pos = 100, 56
+alien.width = 200
+alien.height = 200
+
+
+WIDTH = 500
+HEIGHT = alien.height + 20
 
 
 def draw():
     screen.fill((128, 0, 0))
+    # screen.clear()
+    alien.draw()
 
 
 pgzrun.go()
