771426d78fb396810c743908d7c0306943dc4df9
jackyniu <jackyniu@tencent.com>
2023-09-28 17:26:24 +0800

    apple shoot

---
+++ appleshoot/main.py
@@ -0,0 +1,36 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import pgzrun
+from random import randint
+
+from pgzero.actor import Actor
+
+apple = Actor("apple")
+def place_apple():
+    apple.x = randint(10, 800)
+    apple.y = randint(10, 600)
+
+def on_mouse_down(pos):
+    if apple.collidepoint(pos):
+        print("Good shot!")
+        place_apple()
+    else:
+        print("You missed!")
+        quit()
+
+def draw():
+    screen.clear()
+    apple.draw()
+
+pgzrun.go()
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+
+
